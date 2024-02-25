9960c8807b4906675cb7555e068eb96b8b03a6ec
Farid Tabatabaie <farid.t@gmail.com>
2024-02-24 21:10:01 -0800

    Rename file and add effects

--- sandbox/led_matrix_effects/led_matrix_effects.ino
+++ sandbox/led_matrix_effects/led_matrix_effects.ino
@@ -73,23 +73,45 @@ void setup() {
 }
 
 void loop() {
-  DisplayPic(heart_full, 1000);
+  int d = 50;
+  bool s = 1;
   
-  // scroll from left edge to right edge
-  ScrollH(heart_hollow, 50, DirH::lr);
-  // scroll from right edge to left edge
-  ScrollH(heart_hollow, 50, DirH::rl);
+  SweepLineV(3, 3, 3, s, d);
+  SweepLineH(2, 3, 4, s, d);
+  SweepLineV(5, 2, 4, s, d);
+  SweepLineH(5, 5, 2, s, d);
+  SweepLineV(1, 5, 1, s, d);
+  SweepLineH(0, 1, 6, s, d);
+  SweepLineV(7, 0, 6, s, d);
+  SweepLineH(7, 7, 0, s, d);
 
-  // scroll from top edge to bottom edge
-  ScrollV(smile, 50, DirV::ud);
+  SweepLineV(0, 0, 6, s, d);
+  SweepLineH(6, 1, 6, s, d);
+  SweepLineV(6, 5, 1, s, d);
+  SweepLineH(1, 5, 2, s, d);
+  SweepLineV(2, 2, 4, s, d);
+  SweepLineH(4, 3, 4, s, d);
+  SweepLineV(4, 3, 3, s, d);
+
+  for (int i = 3; i >= 0; i--) {
+    DrawSolidSquare(i, i, 7 - i, 7-i, s, 4 * d);
+  }
+  
+  DisplayPic(heart_full, 10 * d);
+  
+  // Scroll from top edge to bottom edge
+  ScrollV(smile, d, DirV::ud);
   // scroll from bottom edge to top edge
-  ScrollV(smile, 50, DirV::du);
+  ScrollV(smile, d, DirV::du);
+
+  // Scroll from left edge to right edge
+  ScrollH(heart_hollow, d, DirH::lr);
+  // scroll from right edge to left edge
+  ScrollH(heart_hollow, d, DirH::rl);
 
   // Left to right while bobbing up and down
-  BobScrollH(smile_up, smile_down, 100, lr);
-  BobScrollH(smile_up, smile_down, 100, rl);
-
-
+  BobScrollH(smile_up, smile_down, 2 * d, lr);
+  BobScrollH(smile_up, smile_down, 2 * d, rl);
 }
 
 void DisplayPic(byte pic[8], int d) {
@@ -104,13 +126,13 @@ void DisplayPic(byte pic[8], int d) {
 void ScrollH(byte pic[8], int d, DirH dir) {
   byte frame[8] = {};
 
-  for (int k = 0; k < 16; k++) {
+  for (int k = 0; k <= 16; k++) {
     for (int i = 0; i < 8; i++) {
     // Determine the shift operation based on the direction
     if (dir == rl) {
-        frame[i] = k <  8 ? pic[i] >> (8 - k) : pic[i] << (k -  8);
+        frame[i] = k <= 8 ? pic[i] >> (8 - k) : pic[i] << (k - 8);
       } else {
-        frame[i] = k <  8 ? pic[i] << (8 - k) : pic[i] >> (k -  8);
+        frame[i] = k <= 8 ? pic[i] << (8 - k) : pic[i] >> (k - 8);
       }
     }
     DisplayPic(frame, d);
@@ -121,15 +143,15 @@ void BobScrollH(byte pic_1[8], byte pic_2[8], int d, DirH dir) {
   byte frame[8] = {};
   byte *pic; // Pointer to the current array
 
-  for (int k =  0; k <  16; k++) {
+  for (int k =  0; k <= 16; k++) {
     // Alternate between pic_1 and pic_2 every other iterations
     pic = ((pic == pic_1) ? pic_2 : pic_1);
     for (int i =  0; i <  8; i++) {
       // Determine the shift operation based on the direction
       if (dir == rl) {
-        frame[i] = k <  8 ? pic[i] >> (8 - k) : pic[i] << (k -  8);
+        frame[i] = k <= 8 ? pic[i] >> (8 - k) : pic[i] << (k - 8);
       } else {
-        frame[i] = k <  8 ? pic[i] << (8 - k) : pic[i] >> (k -  8);
+        frame[i] = k <= 8 ? pic[i] << (8 - k) : pic[i] >> (k - 8);
       }
     }
     DisplayPic(frame, d);
@@ -176,4 +198,53 @@ void ScrollV(byte pic[8], int d, DirV dir) {
       DisplayPic(frame, d);
     }
   }
+}
+
+void SweepLineH(int y, int x_start, int x_end, bool turn_on, int d) {
+  int x = x_start;
+  bool done = !(y >= 0 && y < 8 && x_start >= 0 && x_start < 8 && x_end >= 0 && x_end < 8);
+  while (!done) {
+    lc.setLed(0, x, y, turn_on);
+    delay(d);
+    x += (x_start <= x_end ? 1 : -1);
+    done = ((x_start <= x_end && x > x_end) || (x_start >= x_end && x < x_end));
+  }
+}
+
+void SweepLineV(int x, int y_start, int y_end, bool turn_on, int d) {
+  int y = y_start;
+  bool done = !(x >= 0 && x < 8 && y_start >= 0 && y_start < 8 && y_end >= 0 && y_end < 8);
+  while (!done) {
+    lc.setLed(0, x, y, turn_on);
+    delay(d);
+    y += (y_start <= y_end ? 1 : -1);
+    done = ((y_start <= y_end && y > y_end) || (y_start >= y_end && y < y_end));
+  }
+}
+
+void DrawSolidSquare(int top_l_x, int top_l_y, int bot_r_x, int bot_r_y, bool inverted, int d) {
+  // Validate args
+  if (top_l_x < 0 || top_l_y < 0 || bot_r_x >= 8 || bot_r_y >= 8) {
+      return;
+  }
+
+  // Generate binary array
+  byte bin_arr[8];
+  // Initialize the binary array with all zeros (ones)
+  for (int i = 0; i < 8; ++i) {
+    bin_arr[i] = (inverted ? 0xff : 0x00);
+  }
+
+  // Set the bits inside the square to 1 (0)
+  for (int i = top_l_y; i <= bot_r_y; ++i) {
+    for (int j = top_l_x; j <= bot_r_x; ++j) {
+      if (inverted) {
+        bitClear(bin_arr[i], 7 - j);
+      }
+      else {
+        bitSet(bin_arr[i], 7 - j);
+      }
+    }
+  }
+  DisplayPic(bin_arr, d);
 }
